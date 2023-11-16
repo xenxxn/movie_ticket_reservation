44a75984de84d8dbef807c08de05169df3e80bbf
zenxxn <bunbun2438@naver.com>
2023-11-17 03:29:51 +0900

    feat: reservation 1:N 관계 설정

--- src/main/java/com/ticket/reservation/domain/showtime/entity/Showtime.java
+++ src/main/java/com/ticket/reservation/domain/showtime/entity/Showtime.java
@@ -1,10 +1,14 @@
 package com.ticket.reservation.domain.showtime.entity;
 
 import com.ticket.reservation.domain.movie.entity.Movie;
+import com.ticket.reservation.domain.reservation.Reservation;
 import com.ticket.reservation.domain.room.entity.Room;
 import com.ticket.reservation.domain.showtime.dto.ShowtimeEditInput;
 import com.ticket.reservation.domain.theater.entity.Theater;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +17,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NoResultException;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -41,6 +46,9 @@ public class Showtime {
 
     private LocalDateTime startTime;
     private LocalDateTime endTime;
+
+    @OneToMany(mappedBy = "showtime", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Reservation> reservations = new ArrayList<>();
 
     public void addShowtime(Movie movie, Room room) {
         this.movie = movie;
