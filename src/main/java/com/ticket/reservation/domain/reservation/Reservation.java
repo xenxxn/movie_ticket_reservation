d0ca9c964e50294b00c0bea69892f729966b8d91
zenxxn <bunbun2438@naver.com>
2023-11-17 03:27:02 +0900

    feat: reservation 엔티티 생성

--- src/main/java/com/ticket/reservation/domain/reservation/Reservation.java
+++ src/main/java/com/ticket/reservation/domain/reservation/Reservation.java
@@ -1,19 +1,36 @@
-//package com.ticket.reservation.domain.reservation;
-//
-//import com.ticket.reservation.domain.member.Member;
-//import com.ticket.reservation.domain.movie.entity.Movie;
-//import com.ticket.reservation.domain.showtime.entity.Showtime;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//public class Reservation {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//
-//    @ManyToOne
-//    private Showtime showtime;
-//
-//}
+package com.ticket.reservation.domain.reservation;
+
+import com.ticket.reservation.domain.seat.entity.Seat;
+import com.ticket.reservation.domain.showtime.entity.Showtime;
+
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "RESERVATION")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "RESERVATION_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "SHOWTIME_ID")
+    private Showtime showtime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "SEAT_ID")
+    private Seat seat;
+
+    @Column(name = "STATUS")
+    private String status;
+
+
+}
